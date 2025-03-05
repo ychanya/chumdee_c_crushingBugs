8f12df6c6e9efcdf4ec946cdd3ca500289855e83
Ya.chanya <ya.chumdee@gmail.com>
2025-03-05 15:34:10 -0500

    Fixed bugs
    This commit fixed bug #1 and #2.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 let theButtons = document.querySelectorAll('#buttonHolder img'),
 puzzleBoard = document.querySelector('.puzzle-board'),
+puzzlePiecesContainer = document.querySelector('.puzzle-pieces'),
 puzzlePieces = document.querySelectorAll('.puzzle-pieces img'),
 dropZones = document.querySelectorAll('.drop-zone'),
 draggedPiece;
-
 
 function changeBGImage() {
     puzzleBoard.style.backgroundImage = `url(images/backGround${this.id}.jpg)`;
@@ -13,23 +13,34 @@ function handleStartDrag() {
     console.log('started dragging this piece: ', this);
     draggedPiece = this;
 }
-
-function handleDragOver(e) {
-    e.preventDefault(); 
-	console.log('you dragged over me');
-}
-
-function handleDrop(e) {
+function handleDragOver(e){
     e.preventDefault();
-    console.log('dropped something on me');
-    this.appendChild(draggedPiece);
+    console.log('you dragged over me');
+}
+function handleDrop(e){
+    e.preventDefault();
+    if (this.children.length === 0) {
+        this.appendChild(draggedPiece);
+        console.log('dropped something on me');
+    }
+}
+function handleClickBack() {
+    if (
+        this.parentElement != null && this.parentElement.classList.contains("drop-zone")
+    ) {
+        puzzlePiecesContainer.appendChild(this);
+        console.log('started clicking this piece back to the container: ', this);
+    }
 }
 
 
-theButtons.forEach(button => button.addEventListener('click', changeBGImage));
+theButtons.forEach(button => button.addEventListener('click',changeBGImage));
 
-puzzlePieces.forEach(piece => piece.addEventListener('dragstart', handleStartDrag));
+puzzlePieces.forEach(piece => {
+    piece.addEventListener('dragstart', handleStartDrag);
+    piece.addEventListener('click', handleClickBack);
+});
 
 dropZones.forEach(zone => zone.addEventListener('dragover', handleDragOver));
 
-dropZones.forEach(zone => zone.addEventListener('drop', handleDrop));
+dropZones.forEach(zone => zone.addEventListener('drop',handleDrop));
